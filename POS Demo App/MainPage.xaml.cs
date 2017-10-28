de72f4575dab94b16ef8796d3aa9ba712edcd147
jack <pwcasdf@naver.com>
2017-10-28 19:20:44 +0900

    #4 @pwcasdf Listing images is done. Getting file name is required.

--- POS Demo App/MainPage.xaml.cs
+++ POS Demo App/MainPage.xaml.cs
@@ -15,35 +15,53 @@ using Windows.UI.Xaml.Navigation;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace POS_Demo_App
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        static ObservableCollection<Person> persons = new ObservableCollection<Person>();
+
         public MainPage()
         {
             this.InitializeComponent();
 
-            CloudStorageAccount account = new CloudStorageAccount(new StorageCredentials("sa_name", "connection_string"),true);
+            CloudStorageAccount account = new CloudStorageAccount(new StorageCredentials("SA_NAME", "SA_KEY"),true);
             CloudBlobClient blobClient = account.CreateCloudBlobClient();
 
-            var container = blobClient.GetContainerReference("images");
-            //var blobList = container.
+            var container = blobClient.GetContainerReference("NAME_OF_CONTAINER");
+            ListBlobsSegmentedInFlatListing(container);
 
-            List <Person> persons = new List<Person>()
-            {
-                new Person{Name="Anuska Sharama",Age=21,Email="anuska@xyz.com",Image="imagepath"},
-                new Person {Name="Asin",Age=26,Email="asin@xyz.com",Image="asin.jpg"},
-                new Person{Name="Deepika",Age=25,Email="deepika@xyz.com",Image="deepika.jpg"}
-            };
             MenuGridView.ItemsSource = persons;
         }
-
+        
+        async public static Task ListBlobsSegmentedInFlatListing(CloudBlobContainer container)
+        {
+            BlobContinuationToken continuationToken = null;
+            BlobResultSegment resultSegment = null;
+            
+            //Call ListBlobsSegmentedAsync and enumerate the result segment returned, while the continuation token is non-null.
+            //When the continuation token is null, the last page has been returned and execution can exit the loop.
+            do
+            {
+                //This overload allows control of the page size. You can return all remaining results by passing null for the maxResults parameter,
+                //or by calling a different overload.
+                resultSegment = await container.ListBlobsSegmentedAsync("", true, BlobListingDetails.All, 10, continuationToken, null, null);
+                foreach (var blobItem in resultSegment.Results)
+                {
+                    persons.Add(new Person { Name = "dd", Age = 12, Email = "dd", Image = blobItem.StorageUri.PrimaryUri.ToString() });
+                }
+                
+                //Get the continuation token.
+                continuationToken = resultSegment.ContinuationToken;
+            }
+            while (continuationToken != null);
+        }
+        
         private void OnMenuClick(object sender, ItemClickEventArgs e)
         {
 
